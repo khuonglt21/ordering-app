11ce0ab99ddf785b4740a968a52ccc8b24fb08d5
khuonglt21 <khuong.kyuubi@gmail.com>
2023-04-28 09:05:52 +0700

    Init connect, repository for mongodb

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,3 @@
-export * from './common.module';
-export * from './common.service';
+export * from './database/database.module';
+export * from './database/abstract.schema';
+export * from './database/abstract.repository';
