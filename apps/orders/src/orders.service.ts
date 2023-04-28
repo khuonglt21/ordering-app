241ecfe29a1e1d701a4fd8289e4ac39d805e77c3
khuonglt21 <khuong.kyuubi@gmail.com>
2023-04-28 11:46:48 +0700

    Feature create order

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create_order_request.dto';
+import { OrdersRepository } from './order.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly ordersRepository: OrdersRepository) {}
+  async createOrder(request: CreateOrderRequest) {
+    return this.ordersRepository.create(request);
   }
 }
