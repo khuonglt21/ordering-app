7996f64ce3bf1f60788c2e0b68f09239d1cb0796
khuonglt21 <khuong.kyuubi@gmail.com>
2023-04-28 15:08:59 +0700

    Connect service order with service billing

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderRequest } from './dto/create_order_request.dto';
 import { OrdersRepository } from './order.repository';
+import { ClientProxy } from '@nestjs/microservices';
+import { BILLING_SERVICE } from './constants/services';
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
   async createOrder(request: CreateOrderRequest) {
     return this.ordersRepository.create(request);
--- libs/common/src/rmq/rmq.module.ts
+++ libs/common/src/rmq/rmq.module.ts
@@ -1,8 +1,37 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { RmqService } from './rmq.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+interface RmqModuleOption {
+  name: string;
+}
 
 @Module({
   providers: [RmqService],
   exports: [RmqService],
 })
-export class RmqModule {}
+export class RmqModule {
+  // turn RmqModule to Dynamic Module
+  static register({ name }: RmqModuleOption): DynamicModule {
+    return {
+      module: RmqModule,
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name,
+            useFactory: (configService: ConfigService) => ({
+              transport: Transport.RMQ,
+              options: {
+                urls: [configService.get<string>('RABBIT_MQ_URI')],
+                queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+              },
+            }),
+            inject: [ConfigService],
+          },
+        ]),
+      ],
+      exports: [ClientsModule],
+    };
+  }
+}
