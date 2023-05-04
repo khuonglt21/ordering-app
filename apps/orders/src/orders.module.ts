237678c0c129ac00842dc3f25b5509b5750fb3fb
khuonglt21 <khuong.kyuubi@gmail.com>
2023-05-04 15:42:41 +0700

    Update auth guard for orders and billing services

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -3,7 +3,7 @@ import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-import { DatabaseModule, RmqModule } from '@app/common';
+import { DatabaseModule, RmqModule, AuthModule } from '@app/common';
 import { OrdersRepository } from './order.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/order.schema';
@@ -21,6 +21,7 @@ import { BILLING_SERVICE } from './constants/services';
     DatabaseModule,
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
     RmqModule.register({ name: BILLING_SERVICE }),
+    AuthModule,
   ],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
